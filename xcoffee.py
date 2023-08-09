04562d499c419044b62b62ceae0b8702418843a7
Michał Rawlik <michal.rawlik@psi.ch>
2023-08-09 23:47:14 +0200

    Use a permanent database.

--- xcoffee.py
+++ xcoffee.py
@@ -1,65 +1,105 @@
 import pywebio
+import sqlite3
 from functools import partial
 from pywebio.input import actions, input, FLOAT, TEXT
 from pywebio.output import popup, close_popup, put_table, put_column, put_text, put_button, put_buttons, clear
 
-users = {"Michał": 0}
 price = 1.5
+
+def getusers():
+    con = sqlite3.connect("xcoffee.db")
+    cur = con.cursor()
+    res = cur.execute("SELECT * FROM users ORDER BY name ASC").fetchall()
+    con.close()
+
+    return res
+
 
 def xcoffee():
     clear()
     put_table(
-        [["User", "Balance", "Actions"]] + \
+        [["User", "Balance", "Total", "Actions"]] + \
         [
             [
                 put_text(s),
-                f"{users[s]}",
+                put_text(f"{balance}"),
+                put_text(f"{total}"),
                 put_buttons(
                     [
                         dict(label="☕", value="☕", color="success"),
                         dict(label="pay", value="pay", color="warning"),
-                        dict(label="delete", value="delete", color="danger")
+                        #dict(label="delete", value="delete", color="danger")
                     ],
                     onclick=partial(rowaction, user=s)) ]
-            for s in users
+            for s, balance, total in getusers()
         ] + \
-        [[ put_button("add name", onclick=adduser), put_text(""), put_text("")]])
+        [[ put_button("add name", onclick=adduser)]])
 
 
 def rowaction(action, user):
     if action == "☕":
-        if user in users.keys():
-            users[user] += 1
+        con = sqlite3.connect("xcoffee.db")
+        cur = con.cursor()
+        cur.execute(f"UPDATE users SET balance = balance + 1 WHERE name = '{user}'")
+        cur.execute(f"UPDATE users SET total = total + 1 WHERE name = '{user}'")
+        con.commit()
+        con.close()
         xcoffee()
     elif action == "pay":
+        con = sqlite3.connect("xcoffee.db")
+        cur = con.cursor()
+        balance = cur.execute(f"SELECT balance FROM users WHERE name = '{user}'").fetchone()[0]
+        con.commit()
+        con.close()
         popup(
             title=f"{user} is paying for coffee",
             content=put_column([
-                put_text(f"Please twint {users[user] * price:.2f}Fr (or {users[user] * price / 2:.2f}Fr if you are a student) to Michał (+41 76 787 22 83)."),
+                put_text(f"Please twint {balance * price:.2f}Fr (or {balance * price / 2:.2f}Fr if you are a student) to Michał (+41 76 787 22 83)."),
                 put_buttons(
                     [ dict(label="paid", value="paid", color="success"),
                      dict(label="cancel", value="cancel", color="danger")],
                      onclick=partial(paypopupaction, user=user))]))
     elif action == "delete":
         if input("Password?", type=TEXT) == "KooharkQuoba4":
-            del users[user]
+            con = sqlite3.connect("xcoffee.db")
+            cur = con.cursor()
+            cur.execute(f"DELETE FROM users WHERE name = '{user}'")
+            con.commit()
+            con.close()
             xcoffee()
+
 
 def paypopupaction(action, user):
     if action == "cancel":
         close_popup()
     if action == "paid":
-        users[user] = 0
+        con = sqlite3.connect("xcoffee.db")
+        cur = con.cursor()
+        cur.execute(f"UPDATE users SET balance = 0 WHERE name = '{user}'")
+        con.commit()
+        con.close()
         close_popup()
         xcoffee()
 
 
 def adduser():
     name = input("What is your name?", type=TEXT)
-    if name not in users.keys():
-        users[name] = 0
+    con = sqlite3.connect("xcoffee.db")
+    cur = con.cursor()
+    res = cur.execute(f"SELECT name FROM users WHERE name='{name}'")
+    if res.fetchone() is None:
+        cur.execute(f"INSERT INTO users VALUES ('{name}', 0, 0)")
+        con.commit()
+    con.close()
     xcoffee()
 
 
 if __name__ == '__main__':
-    pywebio.start_server(xcoffee, port=8080)
+    con = sqlite3.connect("xcoffee.db")
+    cur = con.cursor()
+    res = cur.execute("SELECT name FROM sqlite_master WHERE name='users'")
+    if res.fetchone() is None:
+        cur.execute("CREATE TABLE users(name, balance, total)")
+    con.close()
+
+    pywebio.start_server(xcoffee, port=8080, debug=True)
